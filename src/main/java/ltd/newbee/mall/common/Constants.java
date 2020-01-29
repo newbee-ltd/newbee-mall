40a339dfa4d5c4fb89c9589498f91fe02e211c85
十三 <2449207463@qq.com>
2020-01-29 18:01:17 +0800

    :snail: 注释修改

--- src/main/java/ltd/newbee/mall/common/Constants.java
+++ src/main/java/ltd/newbee/mall/common/Constants.java
@@ -31,7 +31,7 @@ public class Constants {
 
     public final static int GOODS_SEARCH_PAGE_LIMIT = 10;//搜索分页的默认条数(每页10条)
 
-    public final static int ORDER_SEARCH_PAGE_LIMIT = 3;//我的订单列表分页的默认条数(每页5条)
+    public final static int ORDER_SEARCH_PAGE_LIMIT = 3;//我的订单列表分页的默认条数(每页3条)
 
     public final static int SELL_STATUS_UP = 0;//商品上架状态
     public final static int SELL_STATUS_DOWN = 1;//商品下架状态
