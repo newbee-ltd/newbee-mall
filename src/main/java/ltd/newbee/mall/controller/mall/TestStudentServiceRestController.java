a327a02fe05086f8f911020d3263a3022981f8cc
chenghongzhen <chizzy0907@>
2021-10-14 11:55:56 +0900

    update student

--- src/main/java/ltd/newbee/mall/controller/mall/TestStudentServiceRestController.java
+++ src/main/java/ltd/newbee/mall/controller/mall/TestStudentServiceRestController.java
@@ -3,13 +3,19 @@ package ltd.newbee.mall.controller.mall;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ltd.newbee.mall.common.Constants;
+import ltd.newbee.mall.common.ServiceResultEnum;
+import ltd.newbee.mall.controller.vo.NewBeeMallUserVO;
+import ltd.newbee.mall.entity.NewBeeMallShoppingCartItem;
 import ltd.newbee.mall.entity.Student;
 import ltd.newbee.mall.service.StudentService;
 import ltd.newbee.mall.util.Result;
@@ -34,18 +40,28 @@ public class TestStudentServiceRestController {
 		}
 
 	}
-	
+
 	@RequestMapping(value = "/insertStudent", method = RequestMethod.POST)
 	@ResponseBody
 	public Result insertStudent(@RequestBody Student stu) {
-		long count =studentService.insertStudent(stu);
-		if (count<=0) {
+		long count = studentService.insertStudent(stu);
+		if (count <= 0) {
 			return ResultGenerator.genErrorResult(Constants.FETCH_ERROR, Constants.STUDENT_INSERT_ERROR_MESSAGE);
 		} else {
 			return ResultGenerator.genSuccessResult("挿入した");
 		}
-
 	}
-	
+
+	@PutMapping(value = "/student{studentId}/update")
+	@ResponseBody
+	public Result updateStudent(@PathVariable("studentId") Student student, HttpSession httpSession) {
+		student.setId(student.getId());
+          String updatResult= studentService.updateStudent(student);
+		if (!ServiceResultEnum.SUCCESS.getResult().equals(updatResult)) {
+			return ResultGenerator.genErrorResult(Constants.FETCH_ERROR, Constants.STUDENT_INSERT_ERROR_MESSAGE);
+		} else {
+			return ResultGenerator.genSuccessResult("更新した");
+		}
+	}
 
 }
--- src/main/java/ltd/newbee/mall/service/StudentService.java
+++ src/main/java/ltd/newbee/mall/service/StudentService.java
@@ -20,9 +20,10 @@ public interface StudentService {
 	 * @return 学生list
 	 */
 	ArrayList<Student> getStudentListByName(String name);
-	
+
 	long insertStudent(Student stu);
 
+	String updateStudent(Student student);
 	
 
 }
